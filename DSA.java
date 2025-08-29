85a35acab17d4cbb487c1596478424ebaa238f5e
vishalyadav2858 <vishalyadav2858@gmail.com>
2025-08-30 00:06:10 +0530

    KEEE

--- DSA.java
+++ DSA.java
@@ -1,47 +1,37 @@
 // Sorting ///// ///
 // bubble Sorting
 
-public class Current {
 
-    public static void printarray(int arr[]){
+// dfsvfdgvf
+ 
+public class Current {
+ 
+
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+ " ");
             
         }
 
-    }
+    }      
 
-    public static void main(String[] args) {
-        int arr[] = {7,8,3,1,2};
-
-        //bubble sort
+    public  static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};  
+    
+        //bubble sort    
         for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length-i-1; j++) {
-                if (arr[j]>arr[j+1]) {
+            for (int j  = 0; j < arr.length-i-1; j++) {
+                if (arr[j]>a rr[j+1]) {
+  
+                    //swa p  
 
-                    //swap
-                    int temp= arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1]= temp;
-                    
-                }
+                 
+
+             
+
                 
             }
             
-        }
-        printarray(arr);
-    }
-} 
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 
@@ -78,11 +68,6 @@ public class Current {
             arr[smallest] = arr[i];
             arr[i] = temp;
         }
-
-        printarray(arr);
-    }
-}
-
 
 
 
