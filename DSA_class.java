eb4dd439c97a1117bb60ebf9f297636127700b01
vishalyadav2858 <vishalyadav2858@gmail.com>
2025-09-04 11:57:58 +0530

    LK

--- DSA_class.java
+++ DSA_class.java
@@ -1,84 +1,39 @@
-// selection and merge sort
+// // selection and merge sort
 
-public class DSA_class {
+// public class DSA_class {
 
-    public static void printarray(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+//     public static void printarray(int arr[]) {
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.print(arr[i] + " ");
 
-        }
-        System.out.println();
+//         }
+//         System.out.println();
 
-    }
+//     }
 
-    public static void main(String[] args) {
-        int arr[] = { 7, 8, 3, 1, 2 };
+//     public static void main(String[] args) {
+//         int arr[] = { 7, 8, 3, 1, 2 };
 
-        // slection sort
-        for (int i = 0; i < arr.length - 1; i++) {
-            int smallest = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[smallest] > arr[j]) {
-                    smallest = j;
+//         // slection sort
+//         for (int i = 0; i < arr.length - 1; i++) {
+//             int smallest = i;
+//             for (int j = i + 1; j < arr.length; j++) {
+//                 if (arr[smallest] > arr[j]) {
+//                     smallest = j;
 
-                }
+//                 }
 
-            }
+//             }
 
-            // swap
-            int temp = arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i] = temp;
-        }
+//             // swap
+//             int temp = arr[smallest];
+//             arr[smallest] = arr[i];
+//             arr[i] = temp;
+//         }
 
-        printarray(arr);
-    }
-}
-
-
-
-
-
-// selection and merge sort
-
-public class DSA_class {
-
-    public static void printarray(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-
-        }
-        System.out.println();
-
-    }
-
-    public static void main(String[] args) {
-        int arr[] = { 7, 8, 3, 1, 2 };
-
-        // slection sort
-        for (int i = 0; i < arr.length - 1; i++) {
-            int smallest = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[smallest] > arr[j]) {
-                    smallest = j;
-
-                }
-
-            }
-
-            // swap
-            int temp = arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i] = temp;
-        }
-
-        printarray(arr);
-    }
-}
-
-
-
-
+//         printarray(arr);
+//     }
+// }
 
 
 
